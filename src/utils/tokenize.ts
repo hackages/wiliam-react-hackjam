f08cb29095a75e0baaf226a23e5f5374020b9f36
SkyzohKey <skyzohkey@gmail.com>
2020-11-26 23:15:29 +0100

    fix(tokenizer): More way to compare tokens

--- src/utils/tokenize.ts
+++ src/utils/tokenize.ts
@@ -1,7 +1,7 @@
 import { STOP_WORDS } from "../constants/stopWords";
 
-export const tokenize = (str: string) => {
-  return str
+export const tokenize = (terms: string) => {
+  return terms
     .toLowerCase()
     .split(' ')
     .filter(token => STOP_WORDS.includes(token) === false);
@@ -11,7 +11,9 @@ export const compareTokens = (tokens1: string[], tokens2: string[]) => {
   return tokens1.filter(token => {
     return (
       tokens2.includes(token) ||
-      tokens2.indexOf(token) !== -1
+      tokens2.indexOf(token) !== -1 ||
+      tokens2.find(t => t.startsWith(token)) ||
+      tokens2.find(t => t.endsWith(token))
     );
   });
 }
