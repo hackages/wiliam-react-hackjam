62c6d7887061fbc9177bcb91cb201b3749b7b8f7
SkyzohKey <skyzohkey@gmail.com>
2020-11-26 23:18:09 +0100

    chore(tests): Add jest-react-hook-shallow to fix an issue with enzyme
    See the relevant issue here: https://github.com/enzymejs/enzyme/issues/2086

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,5 +1,7 @@
 import 'jest-enzyme';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import enableHooks from 'jest-react-hooks-shallow';
 
 enzyme.configure({ adapter: new Adapter() });
+enableHooks(jest);
