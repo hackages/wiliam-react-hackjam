847c9faaf8a4c8b949bdfe6b6982fbedb7dedd66
SkyzohKey <skyzohkey@gmail.com>
2020-11-27 16:11:10 +0100

    fix(tests): Remove jest-react-hooks-shallow init

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,7 +1,7 @@
 import 'jest-enzyme';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import enableHooks from 'jest-react-hooks-shallow';
+// import enableHooks from 'jest-react-hooks-shallow';
 
 enzyme.configure({ adapter: new Adapter() });
-enableHooks(jest);
+// enableHooks(jest);
