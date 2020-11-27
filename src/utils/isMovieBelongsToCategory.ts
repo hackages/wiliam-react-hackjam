41f8d6d03bc2081e17d910f21e56ebc41bb1ae09
SkyzohKey <skyzohkey@gmail.com>
2020-11-27 17:15:21 +0100

    feat(Categories): Implement `isMovieBelongsToCategory`

--- src/utils/isMovieBelongsToCategory.ts
+++ src/utils/isMovieBelongsToCategory.ts
@@ -1,4 +1,15 @@
-import {IGenre, IMovie} from '../types';
+import { IGenre, IMovie } from '../types';
 
 // a category name should match a genre name
-export const isMovieBelongsToCategory = (genres: IGenre[]) => (movie: IMovie, categoryName: string): boolean => false
+export const isMovieBelongsToCategory = (genres: IGenre[]) => {
+  return (movie: IMovie, categoryName: string): boolean => {
+    if (categoryName === 'All') {
+      return true;
+    }
+
+    const movieGenres = movie.genre_ids.map(id => genres.find(genre => genre.id === id));
+    const results = movieGenres.find(genre => genre?.name === categoryName);
+
+    return results != null;
+  }
+}
