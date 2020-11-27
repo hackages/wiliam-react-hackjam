bd4b18f690a21b85a53b3dfb696959f35e713929
SkyzohKey <skyzohkey@gmail.com>
2020-11-27 17:14:58 +0100

    chore(style): Remove boring logs

--- src/utils/isMovieTitleContain.ts
+++ src/utils/isMovieTitleContain.ts
@@ -6,13 +6,13 @@ export const isMovieTitleContain = (movie: IMovie, searchTerms: string): boolean
   const titleTokens = tokenize(movie.title);
   const tokensMatches = compareTokens(searchTokens, titleTokens);
 
-  console.log(
-    'Movie title:', `"${movie.title}"`, '\n',
-    'Title tokens:', titleTokens, '\n',
-    'Search terms:', `"${searchTerms}"`, '\n',
-    'Search tokens:', searchTokens, '\n',
-    'Tokens matches:', tokensMatches
-  );
+  // console.log(
+  //   'Movie title:', `"${movie.title}"`, '\n',
+  //   'Title tokens:', titleTokens, '\n',
+  //   'Search terms:', `"${searchTerms}"`, '\n',
+  //   'Search tokens:', searchTokens, '\n',
+  //   'Tokens matches:', tokensMatches
+  // );
 
   return tokensMatches.length > 0;
 };
